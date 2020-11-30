a5be178b1441d8031f3a27838bf9aa1ad27bfed5
Lucas-Geremias <geremiaslucas2012@gmail.com>
2020-11-30 15:20:56 -0300

    validation

--- src/main/java/com/av2/kanban/domain/Project.java
+++ src/main/java/com/av2/kanban/domain/Project.java
@@ -2,12 +2,20 @@ package com.av2.kanban.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
 
 @Entity
 public class Project {
@@ -15,13 +23,22 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "Project Name is required")
 	private String projectName;
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min = 4, max = 5, message = "Please use 4 to 5 Characters!")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "Project description is required")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
 	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	public Project() {
--- src/main/java/com/av2/kanban/web/ProjectController.java
+++ src/main/java/com/av2/kanban/web/ProjectController.java
@@ -1,14 +1,20 @@
 package com.av2.kanban.web;
 
+
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.av2.kanban.domain.Project;
+import com.av2.kanban.service.MapValidationErrorService;
 import com.av2.kanban.service.ProjectService;
 
 @RestController
@@ -18,8 +24,16 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if(errorMap!=null) return errorMap;
+		
+		Project project1 = projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 	}
 }
