97bee3cd8f4913b6fa83b3916fede026df9c3376
Lucas-Geremias <geremiaslucas2012@gmail.com>
2020-11-30 16:34:28 -0300

    Find all

--- src/main/java/com/av2/kanban/web/ProjectController.java
+++ src/main/java/com/av2/kanban/web/ProjectController.java
@@ -47,6 +47,8 @@ public class ProjectController {
 		return new ResponseEntity<Project>(project, HttpStatus.OK);
 	}
 	
+	@GetMapping("/all")
+	public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
 	
 	
 	
